3a40406c6080d6682ed6966eb7c17f2f48f3fdec
Fernández Micaelai <porokawaiilol@gmail.com>
2021-10-14 09:59:48 -0300

    prueba hero

--- src/components/HeroContainer/HeroContainer.js
+++ src/components/HeroContainer/HeroContainer.js
@@ -3,12 +3,16 @@ import CssBaseline from '@mui/material/CssBaseline';
 import Container from '@mui/material/Container';
 import './HeroContainer.css';
 import maimoText from '../../images/maimoBlack.png';
+import background from '../../images/01.jpg'
 
 export default function HeroContainer({ children }) {
   return (
     <React.Fragment>
       <CssBaseline />
-      <Container maxWidth="xl">
+      <Container style={{
+        background: 'linear-gradient(to right, #fafcff, #fafcff00)', backgroundImage:
+          `url(${background})`
+      }} maxWidth="xl">
         {children}
         <div id='logo'>
           <img src={maimoText} alt='Maimo News'></img>
