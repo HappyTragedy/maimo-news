2d8cc6784730ccf11efd2eb109530a833c4a8a28
Fernández Micaelai <porokawaiilol@gmail.com>
2021-10-13 21:28:18 -0300

    terminado

--- src/components/MenuDrawer/MenuDrawer.js
+++ src/components/MenuDrawer/MenuDrawer.js
@@ -1,24 +1,30 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
 import Drawer from '@mui/material/Drawer';
-import ArrowRightIcon from '@mui/icons-material/ArrowRight';
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
 import { Link } from 'react-router-dom'
 import './MenuDrawer.css'
+import SportsFootballIcon from '@mui/icons-material/SportsFootball';
+import WorkIcon from '@mui/icons-material/Work';
+import ComputerIcon from '@mui/icons-material/Computer';
+import TheaterComedyIcon from '@mui/icons-material/TheaterComedy';
+import FlagIcon from '@mui/icons-material/Flag';
+import DesignServicesIcon from '@mui/icons-material/DesignServices';
+import GamepadIcon from '@mui/icons-material/Gamepad';
 
 export default function TemporaryDrawer({ isOpen, toggleDrawer }) {
 
     const secciones = [
-        { label: 'Deportes', url: 'deportes'},
-        { label: 'Politica', url: 'politica' },
-        { label: 'Tecnologia', url: 'tecnologia' },
-        { label: 'Espectaculos', url: 'espectaculos' },
-        { label: 'Internacionales', url: 'internacionales' },
-        { label: 'Diseño', url: 'diseño' },
-        { label: 'Gaming', url: 'gaming' }
+        { label: 'Deportes', url: 'deportes', icon: <SportsFootballIcon style={{fontSize:'large', fill:'white'}}/>},
+        { label: 'Politica', url: 'politica', icon: <WorkIcon style={{fontSize:'large', fill:'white'}}/> },
+        { label: 'Tecnologia', url: 'tecnologia', icon: <ComputerIcon style={{fontSize:'large', fill:'white'}}/>},
+        { label: 'Espectaculos', url: 'espectaculos', icon: <TheaterComedyIcon style={{fontSize:'large', fill:'white'}}/> },
+        { label: 'Internacionales', url: 'internacionales', icon:  <FlagIcon style={{fontSize:'large', fill:'white'}}/>},
+        { label: 'Diseño', url: 'diseño', icon: <DesignServicesIcon style={{fontSize:'large', fill:'white'}}/> },
+        { label: 'Gaming', url: 'gaming', icon: <GamepadIcon style={{fontSize:'large', fill:'white'}}/> }
     ]
 
     const list = (anchor) => (
@@ -30,11 +36,11 @@ export default function TemporaryDrawer({ isOpen, toggleDrawer }) {
             onKeyDown={toggleDrawer}
         >
             <List>
-                {secciones.map(({ label, url }) =>//Un map donde se dice que para cada una de las secciones genera el link correspondiente de acuerdo a lo seleccionado.
+                {secciones.map(({ label, url, icon }) =>//Un map donde se dice que para cada una de las secciones genera el link correspondiente de acuerdo a lo seleccionado.
                     <Link to={`/category/${url}`}>
                         <ListItem button>
                             <ListItemIcon>
-                                <ArrowRightIcon style={{fontSize:'large', fill:'white'}}/>
+                                {icon}
                             </ListItemIcon>
                             <ListItemText primary={label} />
                         </ListItem>
